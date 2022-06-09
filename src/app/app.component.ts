2e047abcd9a3d83b46db1ca6fd008959af83e3e7
ahmed807 <ahmedcicsap@outlook.com>
2022-06-09 18:50:12 +0200

    angular swiper patreon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,77 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { SwiperComponent } from "swiper/angular";
+
+// import Swiper core and required components
+import SwiperCore, { Autoplay, Navigation, Pagination, SwiperOptions } from "swiper";
+
+// install Swiper components
+SwiperCore.use([Autoplay, Navigation, Pagination]);
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "app.component.html",
+  styleUrls:["app.component.scss"]
 })
+
 export class AppComponent {
-  title = 'angular-swiper';
+  config:SwiperOptions = {
+    grabCursor: true,
+    watchSlidesProgress: true,
+    loop: true,
+    loopedSlides: 5,
+    slidesPerView: "auto",
+    centeredSlides: true,
+    pagination:true,
+    // navigation:true,
+    autoplay: {
+      delay: 2000,
+      disableOnInteraction:false
+    },
+    on: {
+      // eslint-disable-next-line
+      progress(swiper:any) {
+        const scaleStep = 0.2;
+        const zIndexMax = swiper.slides.length;
+        for (let i = 0; i < swiper.slides.length; i += 1) {
+          const slideEl = swiper.slides[i];
+          const slideProgress = swiper.slides[i].progress;
+          const absProgress = Math.abs(slideProgress);
+          let modify = 1;
+          if (absProgress > 1) {
+            modify = (absProgress - 1) * 0.3 + 1;
+          }
+          const opacityEls = slideEl.querySelectorAll(
+            ".carousel-slider-animate-opacity"
+          );
+          const translate = `${slideProgress * modify * 50}%`;
+          const scale = 1 - absProgress * scaleStep;
+          const zIndex = zIndexMax - Math.abs(Math.round(slideProgress));
+          slideEl.style.transform = `translateX(${translate}) scale(${scale})`;
+          slideEl.style.zIndex = zIndex;
+          if (absProgress > 3) {
+            slideEl.style.opacity = 0;
+          } else {
+            slideEl.style.opacity = 1;
+          }
+
+          opacityEls.forEach((opacityEl:any) => {
+            opacityEl.style.opacity = 1 - absProgress / 3;
+          });
+        }
+      },
+      // eslint-disable-next-line
+      setTransition(swiper, duration) {
+        for (let i = 0; i < swiper.slides.length; i += 1) {
+          const slideEl: any = swiper.slides[i] ;
+          const opacityEls = slideEl.querySelectorAll(
+            ".carousel-slider-animate-opacity"
+          );
+          slideEl.style.transitionDuration = `${duration}ms`;
+          opacityEls.forEach((opacityEl:any) => {
+            opacityEl.style.transitionDuration = `${duration}ms`;
+          });
+        }
+      }
+    }
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
+import { CommonModule } from '@angular/common';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { SwiperModule } from "swiper/angular";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  imports: [BrowserModule, CommonModule, FormsModule, SwiperModule],
+  declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
